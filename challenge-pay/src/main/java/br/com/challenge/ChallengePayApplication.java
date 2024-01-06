595ca67dd18070dc43b522aa22cf52abaaa52c00
Edvaldo Nascimento <aedvaldo.nascimento@gmail.com>
2024-01-05 22:19:00 -0300

    Adiciona integracao a autorizador externo

--- challenge-pay/src/main/java/br/com/challenge/ChallengePayApplication.java
+++ challenge-pay/src/main/java/br/com/challenge/ChallengePayApplication.java
@@ -2,9 +2,11 @@ package br.com.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableFeignClients
 @ComponentScan(basePackages = "br.com.challenge")
 public class ChallengePayApplication {
 
